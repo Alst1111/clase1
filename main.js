110f61f4b66af8e04c1a327ee72330f4c6855d6e
Ale <alejandragonzalez85@gmail.com>
2018-05-12 13:52:58 -0300

    subida de archivos

---
+++ main.js
@@ -0,0 +1,68 @@
+
+/*
+var numero = 1;
+var palabra = "palabras de prueba";
+var boleana = true;
+var nodefinido;
+console.log("var number", numero);
+console.log("var string", palabra);
+console.log("var boolean", boleana);
+console.log("var undefined", nodefinido);
+
+var numero1 = 1;
+var numero2 = 2;
+console.log ("suma", numero1 + numero2);
+console.log("resta", numero1 - numero2);
+console.log("division", numero1 / numero2);
+console.log("multiplicacion", numero1 * numero2);*/
+/*
+var numerocualquiera = 1;
+console.log(numerocualquiera);
+
+var numerocualquiera2 = numerocualquiera++*/
+/*var boleana1 = true;
+var boleana2 = true;
+console.log (boleana1 === boleana2);
+console.log (boleana1 !== boleana2);
+console.log (boleana1 !== boleana2);
+console.log (boleana2 === boleana2);*/
+
+/*var num1 = 10;
+var num2 = 20;
+
+console.log (num1 < num2);
+console.log (num1 <= num1);
+
+
+
+var num1 = 10;
+var num2 = 20;
+
+console.log (num1 < num2);
+*/
+/*function nombreCompleto(nombre,apellido){
+	console.log(nombre + " " + apellido);
+}
+nombreCompleto ("luis", "Vilches");
+*/
+/*function edad(anioNac){
+	return 2018 - anioNac;
+}
+
+console.log(edad(1989));
+*/
+
+ function edad(anioNac){
+	return 2018 - anioNac;
+}
+console.log(edad(1989));
+
+if (edad(1985) >= 18){
+console.log("eres mayor");
+
+}
+else {
+	console.log("eres menor");
+}
+
+
